b49a40e6f169cbde11f7fac73b754ccee1a212a5
phoebe lartisant <phoebelartisant@phoebes-MacBook-Pro.local>
2025-03-30 17:17:27 -0400

    Add: main.c

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ CC = gcc
 CFLAGS = -I/opt/homebrew/include
 LDFLAGS = -L/opt/homebrew/lib -lsoundio -lm
 
-sinewave: 
-	$(CC) -o ./build/sinewave1 sinewave1.c $(CFLAGS) $(LDFLAGS) && $(CC) -o ./build/sinewave2 sinewave2.c $(CFLAGS) $(LDFLAGS)
+main: 
+	$(CC) -o ./build/main main.c $(CFLAGS) $(LDFLAGS)
 
 clean:
-	cd ./build && rm -f sinewave1 && rm -rf sinewave2
+	cd ./build && rm -f main
 
 play:
-	make clean && make sinewave && ./build/sinewave2
+	make clean && make main && ./build/main
--- utils/sinewave2.c
+++ utils/sinewave2.c
@@ -11,7 +11,7 @@
 
 
 static float seconds_offset = 0.0f;
-static void write_callback(struct SoundIoOutStream *outstream,
+static void generate_sinewave(struct SoundIoOutStream *outstream,
         int frame_count_min, int frame_count_max)   
 {
     const struct SoundIoChannelLayout *layout = &outstream->layout;
@@ -67,64 +67,6 @@ static void write_callback(struct SoundIoOutStream *outstream,
         }
 
         frames_left -= frame_count;
-    }
-}
+    };
+};
 
-int main(int argc, char **argv) {
-    int err;
-    struct SoundIo *soundio = soundio_create();
-    if (!soundio) {
-        fprintf(stderr, "out of memory\n");
-        return 1;
-    }
-
-    if ((err = soundio_connect(soundio))) {
-        fprintf(stderr, "error connecting: %s\n", soundio_strerror(err));
-        return 1;
-    }
-
-    soundio_flush_events(soundio);
-
-    int default_out_device_index = soundio_default_output_device_index(soundio);
-    if (default_out_device_index < 0) {
-        fprintf(stderr, "no output device found\n");
-        return 1;
-    }
-
-    struct SoundIoDevice *device = soundio_get_output_device(soundio, default_out_device_index);
-    if (!device) {
-        fprintf(stderr, "out of memory\n");
-        return 1;
-    }
-
-    fprintf(stderr, "Output device: %s\n", device->name);
-
-    struct SoundIoOutStream *outstream = soundio_outstream_create(device);
-    if (!outstream) {
-        fprintf(stderr, "out of memory\n");
-        return 1;
-    }
-    outstream->format = SoundIoFormatFloat32NE;
-    outstream->write_callback = write_callback;
-
-    if ((err = soundio_outstream_open(outstream))) {
-        fprintf(stderr, "unable to open device: %s", soundio_strerror(err));
-        return 1;
-    }
-
-    if (outstream->layout_error)
-        fprintf(stderr, "unable to set channel layout: %s\n", soundio_strerror(outstream->layout_error));
-
-    if ((err = soundio_outstream_start(outstream))) {
-        fprintf(stderr, "unable to start device: %s\n", soundio_strerror(err));
-        return 1;
-    }
-
-    for (;;)
-        soundio_wait_events(soundio);
-
-    soundio_outstream_destroy(outstream);
-    soundio_device_unref(device);
-    soundio_destroy(soundio);
-    return 0;
-}
